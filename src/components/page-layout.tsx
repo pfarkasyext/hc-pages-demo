0cf172b2fd280ba25be2c24aa844110140019db3
Peter Farkas <pfarkas@yext.com>
2024-01-04 16:00:12 -0500

    remove old chat app

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -2,7 +2,12 @@ import * as React from "react";
 import Site from "../types/Site";
 import Header from "./header";
 import Footer from "./footer";
-import { ChatBot } from "./ChatBot";
+import {
+  ChatHeadlessProvider,
+  HeadlessConfig,
+} from "@yext/chat-headless-react";
+import { useState } from "react";
+import { IoCaretDownOutline, IoChatbubblesSharp } from "react-icons/io5";
 
 export type KgPic = {
   url: string;
@@ -14,12 +19,22 @@ type Props = {
   children?: React.ReactNode;
 };
 
+const chatConfig: HeadlessConfig = {
+  botId: "synergic-chat",
+  apiKey: "d646508806b34e8a2b6f01ee937d6698",
+  analyticsConfig: {
+    baseEventPayload: {
+      // internalUser: false, // Boolean identifying whether traffic originated from an internal source.
+      // visitor: {“email”: “user@example.com”} // Map of information about the visitor.
+    },
+  },
+};
+
 const PageLayout = ({ _site, c_siteLogo, children }: Props) => {
   return (
     <div className="min-h-screen">
       <Header _site={_site} c_siteLogoUrl={c_siteLogo?.url} />
       {children}
-      <ChatBot configId={"demo-synergic-health"}></ChatBot>
       <Footer _site={_site}></Footer>
     </div>
   );
